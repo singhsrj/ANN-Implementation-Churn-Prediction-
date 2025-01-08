4be4059d24bd1296736e8776a9f9d87055f83cc0
Suraj Singh <165886618+singhsrj@users.noreply.github.com>
2025-01-08 20:25:30 +0530

    Update app2.py

--- annclassification/app2.py
+++ annclassification/app2.py
@@ -15,7 +15,7 @@ model = tf.keras.models.load_model('annclassification/model.h5')
 with open('annclassification/label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('onehot_encoder_geo.pkl', 'rb') as file:
+with open('annclassification/onehot_encoder_geo.pkl', 'rb') as file:
     onehot_encoder_geo = pickle.load(file)
 
 with open('scaler.pkl', 'rb') as file:
